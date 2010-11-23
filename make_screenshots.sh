702c586d7d0544f87b18c94797334d42f857a1ae
benchoff <benchoff@n3pb.org>
2010-11-23 18:15:04 +0000

    Added some support for Rochester and Berkeley themes.

--- make_screenshots.sh
+++ make_screenshots.sh
@@ -3,6 +3,8 @@
 # Script to make screen shots from the PDF files.
 #
 
+THEMES="Antibes Berkeley Rochester"
+
 createScreenshot() {
         # Extract a page from a pdf as a .png file
         # arguments: pdf-file-name page resolution (dpi)
@@ -19,9 +21,24 @@ createScreenshot() {
         rm $ppmName
 }
 
-createScreenshot vt-beamer-slides 1 150
-createScreenshot vt-beamer-slides 3 150
-createScreenshot vt-beamer-slides 4 150
-createScreenshot vt-beamer-slides 9 150
-createScreenshot vt-beamer-handout 1 300
-createScreenshot vt-beamer-handout 2 300
+for theme in $THEMES; do
+  if [ -d $theme ] ; then
+    rm $theme/*
+  else
+    mkdir $theme
+  fi
+  make realclean
+  make MY_THEME=$theme slides
+  make MY_THEME=$theme handout
+  createScreenshot vt-beamer-slides 1 150
+  createScreenshot vt-beamer-slides 3 150
+  createScreenshot vt-beamer-slides 4 150
+  createScreenshot vt-beamer-slides 9 150
+  createScreenshot vt-beamer-handout 1 300
+  createScreenshot vt-beamer-handout 2 300
+  mv *.png $theme
+  mv *.pdf $theme
+done
+
+exit
+
